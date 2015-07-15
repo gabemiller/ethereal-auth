dea2aa81efbeef0810562258fd9f1d0c3f7980c3
Gabor Molnar <gabormolnar92@gmail.com>
2015-07-15 10:10:53 +0200

    Remove defer load

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -14,13 +14,6 @@ class AuthServiceProvider extends ServiceProvider
 {
 
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
      * Register the service provider.
      *
      * @return void
