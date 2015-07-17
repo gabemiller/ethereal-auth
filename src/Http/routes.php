8c314a5a5f925d5b4755dd5dd548d37e4557f518
Gabor Molnar <gabormolnar92@gmail.com>
2015-07-17 10:24:22 +0200

    Fix route name

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -12,31 +12,37 @@ Route::group(['namespace' => 'Ethereal\Auth\Controllers'], function () {
      * Get admin login view.
      */
     Route::get(trans('ethereal-auth::routes.adminLogin'),
+        ['as' => 'admin.login', 'uses' => 'AuthController@getAdminLogin', 'middleware' => 'guest']);
 
     /**
      * Get site login view.
      */
     Route::get(trans('ethereal-auth::routes.login'),
+        ['as' => 'login.get', 'uses' => 'AuthController@getLogin', 'middleware' => 'guest']);
 
     /**
      * Post all logins.
      */
     Route::post(trans('ethereal-auth::routes.login'),
+        ['as' => 'login.post', 'uses' => 'AuthController@postLogin', 'middleware' => 'guest']);
 
     /**
      * Post all logouts.
      */
     Route::get(trans('ethereal-auth::routes.logout'),
+        ['as' => 'logout.get', 'uses' => 'AuthController@getLogout', 'middleware' => 'auth']);
 
     /**
      * Get site sign up view.
      */
     Route::get(trans('ethereal-auth::routes.signup'),
+        ['as' => 'signup.get', 'uses' => 'AuthController@getSignup', 'middleware' => 'guest']);
 
     /**
      * Post all sign ups.
      */
     Route::post(trans('ethereal-auth::routes.signup'),
+        ['as' => 'signup.post', 'uses' => 'AuthController@postSignup', 'middleware' => 'guest']);
 
 });
 
